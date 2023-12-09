c67089055a619fb3d24d1daa7a79acf1f946490f
ramezHamdiSa'eed <317170274@sha.edu.eg>
2023-12-09 12:49:14 +0200

    create method to pring hellojava and call it in main

---
+++ HelloWorld.java
@@ -0,0 +1,15 @@
+public class HelloWorld{
+	
+	public static void main(String[]Args){
+		printHelloWorld();
+	}
+	static void printHelloWorld(){
+		
+		System.out.println("Hello JAVA");
+
+		
+	}
+	
+	
+	
+}
