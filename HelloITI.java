5ebec1862c59a9be73b4813fd5c3fc9b1cc7c921
ramezHamdiSa'eed <317170274@sha.edu.eg>
2023-12-09 12:51:01 +0200

    create method to print helloITI and call it in HelloITI.main

---
+++ HelloITI.java
@@ -0,0 +1,15 @@
+public class HelloITI{
+	
+	public static void main(String[]Args){
+		printHelloITI();
+	}
+	static void printHelloITI(){
+		
+		System.out.println("Hello ITI");
+
+		
+	}
+	
+	
+	
+}
