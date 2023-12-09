09450051a88ae5493d2ac46f11716a4022b7c93a
ramezHamdiSa'eed <317170274@sha.edu.eg>
2023-12-09 12:55:13 +0200

    rename from HelloWorld.java to HelloSwift.java

--- HelloSwift.java
+++ HelloSwift.java
@@ -1,11 +1,11 @@
-public class HelloWorld{
+public class HelloSwift{
 	
 	public static void main(String[]Args){
 		printHelloWorld();
 	}
 	static void printHelloWorld(){
 		
-		System.out.println("Hello JAVA");
+		System.out.println("Hello SWIFT");
 
 		
 	}
